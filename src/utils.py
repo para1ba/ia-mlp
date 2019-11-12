54633653c482c13e17e0afe4ab17ddd10bd0af73
para1ba <custodio_vitor@outlook.com>
2019-11-12 09:52:29 -0300

    Fixing parse_dataset

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,8 @@ import numpy as np
 def parse_dataset(file):
     data = np.genfromtxt(file, delimiter=',', skip_header=2)
     problem_args = list(np.genfromtxt(file, skip_footer=len(data), dtype=str))
-    classes = int(problem_args[0].split(':')[1])
-    dims = int(problem_args[1].split(':')[1])
+    dims = int(problem_args[0].split(':')[1])
+    classes = int(problem_args[1].split(':')[1])
     dataset = {
         'args': {
             'dims': dims,
