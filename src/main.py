89e892ad711d3ad90c4af14f500aa56a02a2cf2e
para1ba <custodio_vitor@outlook.com>
2019-11-12 09:42:34 -0300

    Setting Structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 import numpy as np
+from utils import parse_dataset
+
+dataset_path = '../res/train.csv'
+model_path = '../res/model.txt'
 
 def main():
-    pass
+    dataset = utils.parse_dataset(dataset_path)
 
+    while True:
+        print("======= MENU =======")
+        print("\t1. Treinar Modelo")
+        print("\t2. Salvar Modelo")
+        print("\t3. Carregar Modelo")
+        print("\t0. Sair")
+        opt = input()
+        break_lines()
+        if opt == '1':
+            pass
+        elif opt == '2':
+            pass
+        elif opt == '3':
+            pass
+        elif opt == '0':
+            break
+        else:
+            print("- OPÇÃO INVÁLIDA -")
 
+def break_lines():
+    for _ in range(20):
+        print('\n')
 
 if __name__ == '__main__':
     main()
